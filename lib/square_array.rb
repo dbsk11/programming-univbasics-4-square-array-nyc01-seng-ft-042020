f7ee8e1ce0ae70dcd9d74bb9ebc17a70c5bb219d
Jessica K <jessikim807@gmail.com>
2020-04-08 15:03:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,9 @@ require 'pry'
 
 def square_array(array)
   counter = 0
+  new_array = []
   while array[counter] do
-    puts array[counter**2]
+    new_array.push(array[counter**2])
     counter += 1
   end
-  binding.pry
 end
-
-square_array(Array)
